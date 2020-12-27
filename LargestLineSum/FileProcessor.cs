33080df20652123bdd41db90c97cf96f20c2ec4b
master2080 <master20802@gmail.com>
2020-12-27 23:37:12 +0200

    Minor fixes.

--- LargestLineSum/FileProcessor.cs
+++ LargestLineSum/FileProcessor.cs
@@ -10,17 +10,17 @@ namespace LargestLineSum
         public string[] GetLinesFromFile(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath))
-                throw new ArgumentNullException($"The file path is empty or null. ");
+                throw new ArgumentNullException("The file path is empty or null. ");
 
             ValidateFileExistsOrThrow(filePath);
             return File.ReadAllLines(filePath);
         }
 
-        private bool ValidateFileExistsOrThrow(string path)
+        private void ValidateFileExistsOrThrow(string path)
         {
             if (!File.Exists(path))
                 throw new FileNotFoundException("The given file is invalid or does not exist. ");
-            return true;
+            
         }
     }
 }
